dda13adcdd7da283f729ca17abf01fa842af7072
navneetar123 <68898569+navneetar123@users.noreply.github.com>
2020-12-21 16:14:35 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,18 @@
+class Box {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      rectMode(CENTER);
+      fill("brown");
+      rect(pos.x, pos.y, this.width, this.height);
+      pop();
+    }
+  };
